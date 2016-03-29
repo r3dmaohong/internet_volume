293a567484f81d60c48e1026df4075345e1c9e8d
r3dmaohong <r3dmaohong@gmail.com>
2016-03-29 15:37:39 +0800

    update

--- rscript/interface_ptt_jiebar.R
+++ rscript/interface_ptt_jiebar.R
@@ -20,3 +20,27 @@ ptt_crawler_jiebar('https://www.ptt.cc/bbs/Therapist/index156.html',110,156)
 ptt_crawler_jiebar('https://www.ptt.cc/bbs/pharmacist/index',229,329)
 ptt_crawler_jiebar('https://www.ptt.cc/bbs/Nurse/index',1000,1150)
 ptt_crawler_jiebar('https://www.ptt.cc/bbs/medache/index',430,476)
+
+
+##整合所有檔案
+#path<-"D:\\abc\\wjhong\\projects\\internet_volume\\output"
+#setwd(path)
+csv_list = list.files(pattern="*.csv")
+for(i in 1:length(csv_list)){
+  temp = read.csv(csv_list[i],stringsAsFactors=F)
+  colnames(temp) = c('詞彙','次數')
+  if(i==1){
+    all_temp = temp
+  }else{
+    all_temp = rbind(all_temp,temp)
+  }
+}
+
+library(plyr)
+temp = ddply(all_temp , '詞彙', summarize, 總次數=sum(次數))
+temp = temp[order(-temp$總次數),]
+
+now = Sys.time()
+now = gsub(':','_',gsub(' ','_',now))
+
+write.csv(temp,paste0(now,'整合詞彙結果.csv'),row.names=F)
