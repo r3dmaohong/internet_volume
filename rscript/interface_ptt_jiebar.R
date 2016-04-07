b7dc704f9adf096eefa1145ef4c40ef302267353
r3dmaohong <r3dmaohong@gmail.com>
2016-04-07 15:22:13 +0800

    upd.

--- rscript/interface_ptt_jiebar.R
+++ rscript/interface_ptt_jiebar.R
@@ -10,23 +10,36 @@ dir.create(file.path(mainDir), showWarnings = FALSE)
 dir.create(file.path(paste0(mainDir,'\\union_output')), showWarnings = FALSE)
 
 source('D:\\abc\\wjhong\\projects\\internet_volume\\rscript\\ptt_jiebar.R', print.eval  = TRUE)
-#source('D:\\abc\\wjhong\\projects\\internet_volume\\rscript\\lineq_jiebar.R', print.eval  = TRUE)
-#source('D:\\abc\\wjhong\\projects\\internet_volume\\rscript\\yahoo++_jiebar.R', print.eval  = TRUE)
+source('D:\\abc\\wjhong\\projects\\internet_volume\\rscript\\lineq_jiebar.R', print.eval  = TRUE)
+source('D:\\abc\\wjhong\\projects\\internet_volume\\rscript\\yahoo++_jiebar.R', print.eval  = TRUE)
 
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Finance/index',750,887,start.time)
+##科大
+if(TRUE){
+  yahoo_crawler_jiebar('https://tw.answers.yahoo.com/search/search_result?p=科大&s=','yahoo科大',1,200,start.time)
+  
+  ##730 848
+  #https://www.ptt.cc/bbs/V_ScHooL/index848.html
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/V_ScHooL/index',730,848,start.time)
+}
 
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/ServiceInfo/index',310,417,start.time)
-
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Salesperson/index',650,762,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/unemployed/index',50,113,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/hairdo/index',1050,1291,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Depstore/index',900,946,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Salary/index',1200,1921,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Broker/index',150,211,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Therapist/index156.html',110,156,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/pharmacist/index',229,329,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/Nurse/index',1000,1150,start.time)
-ptt_crawler_jiebar('https://www.ptt.cc/bbs/medache/index',430,476,start.time)
+##服務業
+if(FALSE){
+  
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Finance/index',750,887,start.time)
+  
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/ServiceInfo/index',310,417,start.time)
+  
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Salesperson/index',650,762,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/unemployed/index',50,113,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/hairdo/index',1050,1291,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Depstore/index',900,946,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Salary/index',1200,1921,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Broker/index',150,211,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Therapist/index156.html',110,156,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/pharmacist/index',229,329,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/Nurse/index',1000,1150,start.time)
+  ptt_crawler_jiebar('https://www.ptt.cc/bbs/medache/index',430,476,start.time)
+}
 
 #lineq_crawler_jiebar(link,forum_name,min,max,start.time)
 #http://lineq.tw/search/question?q=%E8%90%BD%E9%BB%9E&sort=date&sel=all&page=
